eb67ee79bced69b6580c30e8d5e97c91181f17a9
Nathan Santos Barbosa <nathan.s.barbosa0@gmail.com>
2023-03-21 00:37:09 +0000

    Update 6 files

--- src/components/Body/Body.component.tsx
+++ src/components/Body/Body.component.tsx
@@ -1,14 +1,18 @@
-import Section1 from '../Section1/Section1.component'
-import Section2 from '../Section2/Section2.component'
-import './Body.styles.scss'
+import { Section1 } from "../Section1/Section1.component";
+import { Section2 } from "../Section2/Section2.component";
+import "./Body.styles.scss";
 
-export default function Body (){
+interface Props {
+  pkmName: string;
+}
+export default function Body({ pkmName }: Props) {
+  console.log("Nome: ", pkmName);
   return (
     <>
       <div className="body">
-        <Section1/>
-        <Section2/>
+        <Section1 pkmName={pkmName} />
+        <Section2 />
       </div>
     </>
-  )
+  );
 }
--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -1,11 +1,17 @@
-import { FunctionComponent } from 'react'
-import { HeaderProps } from './Header.interface'
-import './Header.styles.scss'
-import { AiOutlineSearch } from 'react-icons/ai'
+import { FunctionComponent } from "react";
+import { HeaderProps } from "./Header.interface";
+import "./Header.styles.scss";
+import { AiOutlineSearch } from "react-icons/ai";
+import { useState } from "react";
+
 export const Header: FunctionComponent<HeaderProps> = ({
   searchTitle,
-  searchDescription
+  searchDescription,
 }) => {
+  const [pkmName, setPkmName] = useState<string>("");
+  function setName() {
+    window.localStorage.setItem("pkmName", pkmName);
+  }
   return (
     <>
       <div className="headerMain">
@@ -13,15 +19,24 @@ export const Header: FunctionComponent<HeaderProps> = ({
           <div className="content1">
             <h2 className="title">{searchTitle}</h2>
             <div className="form">
-              <input type="text" placeholder="Charizard"/>
-              <button><AiOutlineSearch /></button>
+              <input
+                onChange={(e) => {
+                  setPkmName(e.target.value);
+                }}
+                value={pkmName}
+                type="text"
+                placeholder="Charizard"
+              />
+              <button onClick={setName}>
+                <AiOutlineSearch />
+              </button>
             </div>
           </div>
           <div className="content2">
-          <p>{searchDescription}</p>
+            <p>{searchDescription}</p>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
